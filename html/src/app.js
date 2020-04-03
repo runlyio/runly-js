25ab4a957cd965fac0070b250042ae6fabaa42d1
Chad Lee <git@chadly.net>
2020-04-03 09:47:58 -0500

    Rework HTML integration to match docs

--- html/src/app.js
+++ html/src/app.js
@@ -3,17 +3,54 @@ import { render } from "react-dom";
 
 import { RunlyProvider, RunProgress } from "@runly/react-bootstrap";
 
-const runs = document.querySelectorAll("[data-runly-run]");
+const components = {
+	RunProgress
+};
 
-runs.forEach(el => {
-	const org = el.dataset.runlyOrg;
-	const runId = el.dataset.runlyRun;
-	const token = el.dataset.runlyToken;
+const componentsToRender = document.querySelectorAll("[data-runly-component]");
+
+componentsToRender.forEach(el => {
+	const { runlyComponent, runlyToken, ...runlyProps } = el.dataset;
+
+	const ComponentToRender = components[runlyComponent];
+
+	if (!ComponentToRender) {
+		throw new Error(
+			`Unrecognized runly component name: ${runlyComponent}. Check your data-runly-component attribute.`
+		);
+	}
+
+	if (!runlyToken) {
+		throw new Error(
+			`Missing required data attribute runly-token for ${runlyComponent} component. Make sure to include data-runly-token on your HTML elements.`
+		);
+	}
+
+	const props = convertRunlyProps(runlyProps);
+	console.log({ runlyProps, props });
 
 	render(
-		<RunlyProvider accessToken={token}>
-			<RunProgress {...{ org, runId }} />
+		<RunlyProvider accessToken={runlyToken}>
+			<ComponentToRender {...props} />
 		</RunlyProvider>,
 		el
 	);
 });
+
+function convertRunlyProps(runlyProps) {
+	const props = {};
+
+	for (let propName in runlyProps) {
+		if (propName.startsWith("runly")) {
+			props[convertAttrName(propName)] = runlyProps[propName];
+		}
+	}
+
+	return props;
+}
+
+function convertAttrName(name) {
+	let propName = name.substring(5, name.length);
+	propName = propName.charAt(0).toLowerCase() + propName.substring(1);
+	return propName;
+}
