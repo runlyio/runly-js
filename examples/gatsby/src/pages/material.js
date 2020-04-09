d3f47380d4fd1a226b0f669b072427a78b96fbfd
Chad Lee <chad@runly.io>
2020-04-09 13:51:24 -0500

    Implement react-material package (#7)

---
+++ examples/gatsby/src/pages/material.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Layout from "../layout";
+
+import { useLogin } from "@runly/core";
+import { RunProgress } from "@runly/react-material";
+
+const MaterialPage = ({ location }) => {
+	const isAuthenticated = useLogin(location);
+
+	if (!isAuthenticated) {
+		return <span>Authenticating...</span>;
+	}
+
+	const run = { org: "runly", runId: "9e391b9c-fefa-4eda-9693-e32453ba0181" };
+
+	return (
+		<Layout>
+			<RunProgress {...run} />
+		</Layout>
+	);
+};
+
+export default MaterialPage;
---
+++ react-material/README.md
@@ -0,0 +1,7 @@
+# Runly.js React Material UI Integration
+
+> Bootstrap-styled react components to connect to Runly
+
+This package provides a number of [React](https://reactjs.org/) components implemented with [Material UI](https://material-ui.com/) that can be used in your application.
+
+[Learn how to use `@runly/react-material`](https://www.runly.io/docs/integration/js/react-material/) in your application.
